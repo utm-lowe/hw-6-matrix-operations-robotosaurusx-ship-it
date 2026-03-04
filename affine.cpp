5cb89d3a15c99a2058888ed32eea06cfcc4b0c39
Logan Jackson <robotosaurusx@gmail.com>
2026-03-04 04:35:30 +0000

    affine.cpp invalid choice error message was wrong

--- affine.cpp
+++ affine.cpp
@@ -201,7 +201,7 @@ Matrix transformMenu()
             case 'D':
                 break;
             default:
-                cout << "Invalid choice, please try again." << endl;
+                cout << "Invalid choice. Please try again." << endl;
                 break;
         }
 
